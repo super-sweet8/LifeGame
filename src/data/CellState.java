67e56bbbcce900af382dcf5480137263587d7921
2432684827@qq.com <super_sweet>
2020-03-25 14:55:44 +0800

    first commit create two class about cell

---
+++ src/data/CellState.java
@@ -0,0 +1,18 @@
+package data;
+
+/**
+ * 细胞状�??
+ */
+public enum CellState {
+    DEAD(0),
+    LIVE(1);
+    private int value;
+
+    CellState(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
